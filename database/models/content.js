dc7208d4cfadce3a5e7ca927aa18d79841a996f9
Balkar_Singh <balkar81199@gmial.com>
2023-03-09 14:37:01 +0530

    feat: create database

---
+++ database/models/content.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class content extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  content.init({
+    content_name: DataTypes.STRING,
+    columns: DataTypes.JSONB
+  }, {
+    sequelize,
+    modelName: 'content',
+  });
+  return content;
+};
